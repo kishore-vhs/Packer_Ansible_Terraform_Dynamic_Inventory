9432a359d93e82a3c40671e6a614beca7190b812
vhskishore <hemasivakishore@hotmail.com>
2024-03-12 01:48:36 +0530

    Updated Script

--- localfile.tf
+++ localfile.tf
@@ -1,14 +1,14 @@
 resource "local_file" "inventory-file" {
-    content = templatefile("details.tpl",
-        {
-            testserver01    =   aws_instance.srv-1.0.public_ip
-            testserver02    =   aws_instance.srv-2.1.public_ip
-            testserver03    =   aws_instance.srv-3.2.public_ip
-            pvtserver01     =   aws_instance.srv-1.0.private_ip
-            pvtserver02     =   aws_instance.srv-2.1.private_ip
-            pvtserver03     =   aws_instance.srv-3.2.private_ip
-        }
-    )
-    # Below is inventory file.
-    filename = "inventory_file"
+  content = templatefile("details.tpl",
+    {
+      testserver01 = aws_instance.srv.0.public_ip
+      testserver02 = aws_instance.srv.1.public_ip
+      testserver03 = aws_instance.srv.2.public_ip
+      pvtserver01  = aws_instance.srv.0.private_ip
+      pvtserver02  = aws_instance.srv.1.private_ip
+      pvtserver03  = aws_instance.srv.2.private_ip
+    }
+  )
+  # Below is inventory file.
+  filename = "inventory_file"
 }
--- vpc.tf
+++ vpc.tf
@@ -1,21 +1,21 @@
 resource "aws_vpc" "default" {
-    cidr_block = var.cidr
-    enable_dns_hostnames = true
-    tags = {
-        Name = "${var.vpc_name}"
-    }
+  cidr_block           = var.cidr
+  enable_dns_hostnames = true
+  tags = {
+    Name = "${var.vpc_name}"
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
-    vpc_id = "${aws_vpc.default.id}"
-    tags = {
-        Name = "${var.vpc_name}-IGW"
-    }
+  vpc_id = aws_vpc.default.id
+  tags = {
+    Name = "${var.vpc_name}-IGW"
+  }
 }
 
 resource "aws_subnet" "subnet1" {
-  vpc_id = "${aws_vpc.default.id}"
-  cidr_block = var.subnet_1_cidr
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.subnet_1_cidr
   availability_zone = "us-east-1a"
   tags = {
     Name = "${var.vpc_name}-Subnet-1"
@@ -24,8 +24,8 @@ resource "aws_subnet" "subnet1" {
 }
 
 resource "aws_subnet" "subnet2" {
-  vpc_id = "${aws_vpc.default.id}"
-  cidr_block = var.subnet_2_cidr
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.subnet_2_cidr
   availability_zone = "us-east-1b"
   tags = {
     Name = "${var.vpc_name}-Subnet-2"
@@ -34,8 +34,8 @@ resource "aws_subnet" "subnet2" {
 }
 
 resource "aws_subnet" "subnet3" {
-  vpc_id = "${aws_vpc.default.id}"
-  cidr_block = var.subnet_3_cidr
+  vpc_id            = aws_vpc.default.id
+  cidr_block        = var.subnet_3_cidr
   availability_zone = "us-east-1c"
   tags = {
     Name = "${var.vpc_name}-Subnet-3"
@@ -44,22 +44,22 @@ resource "aws_subnet" "subnet3" {
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.default.id}"
-  route = {
+  vpc_id = aws_vpc.default.id
+  route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.igw.id}"
+    gateway_id = aws_internet_gateway.igw.id
   }
 }
 
 resource "aws_route_table_association" "public" {
-  route_table_id = "${aws_route_table.public.id}"
-  subnet_id = "${aws_subnet.subnet1.id}"
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.subnet1.id
 }
 
 resource "aws_security_group" "allow_all" {
   name        = "Allow_All"
   description = "Allow all inbound traffic"
-  vpc_id      = "${aws_vpc.default.id}"
+  vpc_id      = aws_vpc.default.id
 
   ingress {
     from_port   = 0
@@ -69,9 +69,9 @@ resource "aws_security_group" "allow_all" {
   }
 
   egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    }
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
